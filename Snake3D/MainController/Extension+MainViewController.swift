e910aca130db63c878ef47bb0112dcf6c739ae2c
xiaoyu2006 <1783250036@qq.com>
2020-01-23 00:04:03 +0800

    Tiny fix x2.

--- Snake3D/MainController/Extension+MainViewController.swift
+++ Snake3D/MainController/Extension+MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  MainViewControllerARSCNViewDelegate.swift
+//  MainViewControllerARSessionDelegate.swift
 //  Snake3D
 //
 //  Created by Nemo on 2020/1/22.
@@ -8,6 +8,27 @@
 
 import ARKit
 
+// MARK: - ARSessionDelegate
+extension MainViewController: ARSessionDelegate {
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Exit", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+        fatalError(error.localizedDescription)
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        self.sceneView.session.pause()
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        self.resetTracking()
+    }
+}
 
 // MARK: - ARSCNViewDelegate
 extension MainViewController: ARSCNViewDelegate {
